0bb91aecc8de6e9a241db7a192b25506c41fec61
聡 松永 <matsunaga@nextscape.net>
2021-08-13 14:46:10 +0900

    下チャットを実施

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
 
-export default ({ data }) => (
+const About = ({ data }) => (
   <React.Fragment>
     <h2>{data.site.siteMetadata.title}</h2>
     <div>{data.site.siteMetadata.description}</div>
     <Link to="/">Back to home</Link>
   </React.Fragment>
 )
+export default About;
 
 export const query = graphql `
   query {
--- src/pages/chat-test.js
+++ src/pages/chat-test.js
@@ -4,69 +4,102 @@ import { Link, graphql } from 'gatsby';
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import "../components/chat-test.css"
-import Test1 from "../images/test1.png"
-import Test2 from "../images/test2.png"
 import { StaticImage } from "gatsby-plugin-image"
-  
-export default ({data}) => (
- <Layout>
-    <Seo title="Chat" />
-    <div className="chat-box">
-        <div className="chat-face">
-            <StaticImage
-              src="../images/test1.png"
-              width={90}
-              height={90}
-              quality={95}
-              formats={["AUTO", "WEBP", "AVIF"]}
-              alt="自分のチャット画像です。"
-            />
-        </div>
-        <div className="chat-area">
-            <div className="chat-hukidashi">
-            ふきだしなのですーふきだしですーふきだー
-            </div>
-        </div>
-    </div>
-    <div className="chat-box-right">
-        <div className="chat-face-right">
-            <StaticImage
-                  src="../images/test2.png"
-                  width={90}
-                  height={90}
-                  quality={95}
-                  formats={["AUTO", "WEBP", "AVIF"]}
-                  alt="誰かのチャット画像です。"
-                />
-        </div>
-        <div className="chat-area-right">
-            <div className="chat-hukidashi-right someone">
-            ふきだしだよ<br/>
-            へへへ
-            </div>
-        </div>
-    </div>
-    <div className="chat-box-top">
-        <div className="chat-face-top">
-            <StaticImage
-              src="../images/test1.png"
-              width={90}
-              height={90}
-              quality={95}
-              formats={["AUTO", "WEBP", "AVIF"]}
-              alt="自分のチャット画像です。"
-            />
-        </div>
-        <div className="chat-area-top">
-            <div className="chat-hukidashi-top">
-            ふきだしなのですーふきだしですーふきだー
-            </div>
-        </div>
-    </div>
-    
-    <Link to="/">Back to home</Link>
-  </Layout>
-)
+
+const Chat = ({data}) => (
+  <Layout>
+     <Seo title="Chat" />
+     <div className="chat-box">
+         <div className="chat-face">
+             <StaticImage
+               src="../images/test1.png"
+               width={90}
+               height={90}
+               quality={95}
+               formats={["AUTO", "WEBP", "AVIF"]}
+               alt="自分のチャット画像です。"
+             />
+         </div>
+         <div className="chat-area">
+             <div className="chat-hukidashi">
+             ふきだしなのですーふきだしですーふきだー
+             </div>
+         </div>
+     </div>
+     <div className="chat-box-right">
+         <div className="chat-face-right">
+             <StaticImage
+                   src="../images/test2.png"
+                   width={90}
+                   height={90}
+                   quality={95}
+                   formats={["AUTO", "WEBP", "AVIF"]}
+                   alt="誰かのチャット画像です。"
+                 />
+         </div>
+         <div className="chat-area-right">
+             <div className="chat-hukidashi-right someone">
+             ふきだしだよ<br/>
+             へへへ
+             </div>
+         </div>
+     </div>
+     <div className="chat-box-top">
+         <div className="chat-face-top">
+             <StaticImage
+               src="../images/test1.png"
+               width={90}
+               height={90}
+               quality={95}
+               formats={["AUTO", "WEBP", "AVIF"]}
+               alt="自分のチャット画像です。"
+             />
+         </div>
+         <div className="chat-area-top">
+             <div className="chat-hukidashi-top">
+             ふきだしなのですーふきだしですーふきだー
+             </div>
+         </div>
+     </div>
+     <div className="chat-box-top1">
+         <div className="chat-face-top1">
+             <StaticImage
+               src="../images/test1.png"
+               width={90}
+               height={90}
+               quality={95}
+               formats={["AUTO", "WEBP", "AVIF"]}
+               alt="自分のチャット画像です。"
+             />
+         </div>
+         <div className="chat-area-top1">
+             <div className="chat-hukidashi-top1">
+             ふきだしなのですーふきだしですーふきだー
+             </div>
+         </div>
+     </div>
+     <div className="chat-box-bottom1">
+         <div className="chat-area-bottom1">
+             <div className="chat-hukidashi-bottom1">
+             ふきだしなのですーふきだしですーふきだー
+             </div>
+         </div>
+         <div className="chat-face-bottom1">
+             <StaticImage
+               src="../images/test1.png"
+               width={90}
+               height={90}
+               quality={95}
+               formats={["AUTO", "WEBP", "AVIF"]}
+               alt="自分のチャット画像です。"
+             />
+         </div>
+     </div>
+
+     <Link to="/">Back to home</Link>
+   </Layout>
+ )
+export default Chat;
 
 export const query = graphql `
   query {
